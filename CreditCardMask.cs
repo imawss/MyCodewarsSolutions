fa604ac3f74b1ac0c3f8a3ab186c162929514ce9
✦aras <arasarkan9@gmail.com>
2022-08-24 13:03:25 +0300

    Some completed solutions

---
+++ CreditCardMask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class CreditCardMask
+{
+    // Usually when you buy something, you're asked whether your credit card number,
+    // phone number or answer to your most secret question is still correct. However,
+    // since someone could look over your shoulder, you don't want that shown on your screen.
+    // Instead, we mask it.
+    // Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+    // Examples
+    // "4556364607935616" --> "############5616"
+    // "64607935616" -->      "#######5616"
+    // "1"           -->                "1"
+    // ""            -->                 ""
+
+    // "What was the name of your first pet?"
+    // "Skippy" --> "##ippy"
+
+    private const int UnMaskCount = 4;
+
+    // return masked string
+    public static string Maskify(string cc)
+    {
+        StringBuilder sb = new StringBuilder(cc);
+        for (int i = 0; i < sb.Length - UnMaskCount; ++i)
+            sb[i] = '#';
+
+        return sb.ToString();
+    }
+
+}
---
+++ SumOfPositive.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public class SumOfPositive
+{
+
+    //You get an array of numbers, return the sum of all of the positives ones.
+    //Example[1, -4, 7, 12] => 1 + 7 + 12 = 20
+    //Note: if there is nothing to sum, the sum is default to 0.
+
+    public static int PositiveSum(int[] arr)
+    {
+        return arr.Where(x => x > 0).Sum();
+    }
+
+}
